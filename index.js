838e2b46fc361ddefae08720b6c7a0f90a9df750
emblo <emma.blomqvist@outlook.com>
2021-10-23 15:21:33 +0200

    refactored code

--- index.js
+++ index.js
@@ -10,26 +10,49 @@ window.onload = function() {
 //Append tables to this div
 let myTableDiv = document.getElementById("tables");
 
-//ANCIENT TABLE
-buildAncientTable();
+//Era variables
+const ancient = cultures.filter(obj => {
+    return obj.Era === "Ancient";
+});
 
-function buildAncientTable() {
-    //Only returns objects for the Ancient era
-    const ancient = cultures.filter(obj => {
-        return obj.Era === "Ancient";
-    });
-    
+const classical = cultures.filter(obj => {
+    return obj.Era === "Classical";
+});
+
+const medieval = cultures.filter(obj => {
+    return obj.Era === "Medieval";
+});
+
+const earlyModern = cultures.filter(obj => {
+    return obj.Era === "Early Modern";
+});
+
+const industrial = cultures.filter(obj => {
+    return obj.Era === "Industrial";
+});
+
+const contemporary = cultures.filter(obj => {
+    return obj.Era === "Contemporary";
+});
+
+buildTable(ancient);
+buildTable(classical);
+buildTable(medieval);
+buildTable(earlyModern);
+buildTable(industrial);
+buildTable(contemporary);
+
+function buildTable(era) {
     //Create table + headers
-    const ancientTable = document.createElement("table"); 
-    ancientTable.id = "ancient-table";
+    let eraTable = document.createElement("table"); 
     let headers = ["Culture", "Affinity", "Legacy Trait", "Emblematic Unit", "Emblematic Quarter", "Affinity Action", "Affinity Bonus"];
     let headerRow = document.createElement("tr");
 
     //Create and append H2 for table
-    const ancientH2 = document.createElement("h2");
-    const ancientH2Text = document.createTextNode("Ancient Era");
-    ancientH2.appendChild(ancientH2Text);
-    ancientTable.appendChild(ancientH2);
+    const eraH2 = document.createElement("h2");
+    const eraH2Text = document.createTextNode(era[0].Era + " Era");
+    eraH2.appendChild(eraH2Text);
+    eraTable.appendChild(eraH2);
 
     //Headers for table
     headers.forEach(headerText => {
@@ -39,279 +62,24 @@ function buildAncientTable() {
         headerRow.appendChild(header);
     });
 
-    ancientTable.appendChild(headerRow);
+    eraTable.appendChild(headerRow);
 
     //Table data
-    for (let i = 0; i < ancient.length; i++) {
+    for (let i = 0; i < era.length; i++) {
         let row = "<tr>" + 
-                        "<td>" + "<a href='" + "https://www.google.com'" + ">" + ancient[i].Culture + "</a>" + "</td>" +
-                        "<td>" + ancient[i].Affinity + "</td>" +
-                        "<td>" + ancient[i]["Legacy trait"] + "</td>" +
-                        "<td>" + ancient[i]["Emblematic unit"] + "</td>" +
-                        "<td>" + ancient[i]["Emblematic quarter"]+ "</td>" +
-                        "<td>" + ancient[i]["Affinity action"] + "</td>" +
-                        "<td>" + ancient[i]["Affinity bonus"] + "</td>" +
+                        "<td>" + "<a href='" + "https://www.google.com'" + ">" + era[i].Culture + "</a>" + "</td>" +
+                        "<td>" + era[i].Affinity + "</td>" +
+                        "<td>" + era[i]["Legacy trait"] + "</td>" +
+                        "<td>" + era[i]["Emblematic unit"] + "</td>" +
+                        "<td>" + era[i]["Emblematic quarter"]+ "</td>" +
+                        "<td>" + era[i]["Affinity action"] + "</td>" +
+                        "<td>" + era[i]["Affinity bonus"] + "</td>" +
                     "</tr>"
 
         //Append row to table
-        ancientTable.innerHTML += row;
+        eraTable.innerHTML += row;
     };
 
     //Append to div
-    myTableDiv.appendChild(ancientTable);
-};
-
-//CLASSICAL TABLE
-buildClassicalTable();
-
-function buildClassicalTable() {
-    //Only returns objects for the Classical era
-    const classical = cultures.filter(obj => {
-        return obj.Era === "Classical";
-    });
-    
-    //Create table + headers
-    const classicalTable = document.createElement("table"); 
-    classicalTable.id = "classical-table";
-    let headers = ["Culture", "Affinity", "Legacy Trait", "Emblematic Unit", "Emblematic Quarter", "Affinity Action", "Affinity Bonus"];
-    let headerRow = document.createElement("tr");
-
-    //Create and append H2 for table
-    const classicalH2 = document.createElement("h2");
-    const classicalH2Text = document.createTextNode("Classical Era");
-    classicalH2.appendChild(classicalH2Text);
-    classicalTable.appendChild(classicalH2);
-
-    //Headers for table
-    headers.forEach(headerText => {
-        let header = document.createElement("th");
-        let textNode = document.createTextNode(headerText);
-        header.appendChild(textNode);
-        headerRow.appendChild(header);
-    });
-
-    classicalTable.appendChild(headerRow);
-
-    //Table data
-    for (let i = 0; i < classical.length; i++) {
-        let row = "<tr>" + 
-                        "<td>" + "<a href='" + "https://www.google.com'" + ">" + classical[i].Culture + "</a>" + "</td>" +
-                        "<td>" + classical[i].Affinity + "</td>" +
-                        "<td>" + classical[i]["Legacy trait"] + "</td>" +
-                        "<td>" + classical[i]["Emblematic unit"] + "</td>" +
-                        "<td>" + classical[i]["Emblematic quarter"]+ "</td>" +
-                        "<td>" + classical[i]["Affinity action"] + "</td>" +
-                        "<td>" + classical[i]["Affinity bonus"] + "</td>" +
-                    "</tr>"
-
-        //Append row to table
-        classicalTable.innerHTML += row;
-    };
-
-    //Append to div
-    myTableDiv.appendChild(classicalTable);
-};
-
-//MEDIEVAL TABLE
-buildMedievalTable();
-
-function buildMedievalTable() {
-    //Only returns objects for the Medieval era
-    const medieval = cultures.filter(obj => {
-        return obj.Era === "Medieval";
-    });
-    
-    //Create table + headers
-    const medievalTable = document.createElement("table"); 
-    medievalTable.id = "medieval-table";
-    let headers = ["Culture", "Affinity", "Legacy Trait", "Emblematic Unit", "Emblematic Quarter", "Affinity Action", "Affinity Bonus"];
-    let headerRow = document.createElement("tr");
-
-    //Create and append H2 for table
-    const medievalH2 = document.createElement("h2");
-    const medievalH2Text = document.createTextNode("Medieval Era");
-    medievalH2.appendChild(medievalH2Text);
-    medievalTable.appendChild(medievalH2);
-
-    //Headers for table
-    headers.forEach(headerText => {
-        let header = document.createElement("th");
-        let textNode = document.createTextNode(headerText);
-        header.appendChild(textNode);
-        headerRow.appendChild(header);
-    });
-
-    medievalTable.appendChild(headerRow);
-
-    //Table data
-    for (let i = 0; i < medieval.length; i++) {
-        let row = "<tr>" + 
-                        "<td>" + "<a href='" + "https://www.google.com'" + ">" + medieval[i].Culture + "</a>" + "</td>" +
-                        "<td>" + medieval[i].Affinity + "</td>" +
-                        "<td>" + medieval[i]["Legacy trait"] + "</td>" +
-                        "<td>" + medieval[i]["Emblematic unit"] + "</td>" +
-                        "<td>" + medieval[i]["Emblematic quarter"]+ "</td>" +
-                        "<td>" + medieval[i]["Affinity action"] + "</td>" +
-                        "<td>" + medieval[i]["Affinity bonus"] + "</td>" +
-                    "</tr>"
-
-        //Append row to table
-        medievalTable.innerHTML += row;
-    };
-
-    //Append to div
-    myTableDiv.appendChild(medievalTable);
-};
-
-//EARLY MODERN TABLE
-buildEarlyModernTable();
-
-function buildEarlyModernTable() {
-    //Only returns objects for the Early Modern era
-    const earlyModern = cultures.filter(obj => {
-        return obj.Era === "Early Modern";
-    });
-    
-    //Create table + headers
-    const earlyModernTable = document.createElement("table"); 
-    earlyModernTable.id = "early-modern-table";
-    let headers = ["Culture", "Affinity", "Legacy Trait", "Emblematic Unit", "Emblematic Quarter", "Affinity Action", "Affinity Bonus"];
-    let headerRow = document.createElement("tr");
-
-    //Create and append H2 for table
-    const earlyModernH2 = document.createElement("h2");
-    const earlyModernH2Text = document.createTextNode("Early Modern Era");
-    earlyModernH2.appendChild(earlyModernH2Text);
-    earlyModernTable.appendChild(earlyModernH2);
-
-    //Headers for table
-    headers.forEach(headerText => {
-        let header = document.createElement("th");
-        let textNode = document.createTextNode(headerText);
-        header.appendChild(textNode);
-        headerRow.appendChild(header);
-    });
-
-    earlyModernTable.appendChild(headerRow);
-
-    //Table data
-    for (let i = 0; i < earlyModern.length; i++) {
-        let row = "<tr>" + 
-                        "<td>" + "<a href='" + "https://www.google.com'" + ">" + earlyModern[i].Culture + "</a>" + "</td>" +
-                        "<td>" + earlyModern[i].Affinity + "</td>" +
-                        "<td>" + earlyModern[i]["Legacy trait"] + "</td>" +
-                        "<td>" + earlyModern[i]["Emblematic unit"] + "</td>" +
-                        "<td>" + earlyModern[i]["Emblematic quarter"]+ "</td>" +
-                        "<td>" + earlyModern[i]["Affinity action"] + "</td>" +
-                        "<td>" + earlyModern[i]["Affinity bonus"] + "</td>" +
-                    "</tr>"
-
-        //Append row to table
-        earlyModernTable.innerHTML += row;
-    };
-
-    //Append to div
-    myTableDiv.appendChild(earlyModernTable);
-};
-
-//INDUSTRIAL TABLE
-buildIndustrialTable();
-
-function buildIndustrialTable() {
-    //Only returns objects for the Industrial era
-    const industrial = cultures.filter(obj => {
-        return obj.Era === "Industrial";
-    });
-    
-    //Create table + headers
-    const industrialTable = document.createElement("table"); 
-    industrialTable.id = "industrial-table";
-    let headers = ["Culture", "Affinity", "Legacy Trait", "Emblematic Unit", "Emblematic Quarter", "Affinity Action", "Affinity Bonus"];
-    let headerRow = document.createElement("tr");
-
-    //Create and append H2 for table
-    const industrialH2 = document.createElement("h2");
-    const industrialH2Text = document.createTextNode("Industrial Era");
-    industrialH2.appendChild(industrialH2Text);
-    industrialTable.appendChild(industrialH2);
-
-    //Headers for table
-    headers.forEach(headerText => {
-        let header = document.createElement("th");
-        let textNode = document.createTextNode(headerText);
-        header.appendChild(textNode);
-        headerRow.appendChild(header);
-    });
-
-    industrialTable.appendChild(headerRow);
-
-    //Table data
-    for (let i = 0; i < industrial.length; i++) {
-        let row = "<tr>" + 
-                        "<td>" + "<a href='" + "https://www.google.com'" + ">" + industrial[i].Culture + "</a>" + "</td>" +
-                        "<td>" + industrial[i].Affinity + "</td>" +
-                        "<td>" + industrial[i]["Legacy trait"] + "</td>" +
-                        "<td>" + industrial[i]["Emblematic unit"] + "</td>" +
-                        "<td>" + industrial[i]["Emblematic quarter"]+ "</td>" +
-                        "<td>" + industrial[i]["Affinity action"] + "</td>" +
-                        "<td>" + industrial[i]["Affinity bonus"] + "</td>" +
-                    "</tr>"
-
-        //Append row to table
-        industrialTable.innerHTML += row;
-    };
-
-    //Append to div
-    myTableDiv.appendChild(industrialTable);
-};
-
-//CONTEMPORARY TABLE
-buildContemporaryTable();
-
-function buildContemporaryTable() {
-    //Only returns objects for the Industrial era
-    const contemporary = cultures.filter(obj => {
-        return obj.Era === "Contemporary";
-    });
-    
-    //Create table + headers
-    const contemporaryTable = document.createElement("table"); 
-    contemporaryTable.id = "contemporary-table";
-    let headers = ["Culture", "Affinity", "Legacy Trait", "Emblematic Unit", "Emblematic Quarter", "Affinity Action", "Affinity Bonus"];
-    let headerRow = document.createElement("tr");
-
-    //Create and append H2 for table
-    const contemporaryH2 = document.createElement("h2");
-    const contemporaryH2Text = document.createTextNode("Contemporary Era");
-    contemporaryH2.appendChild(contemporaryH2Text);
-    contemporaryTable.appendChild(contemporaryH2);
-
-    //Headers for table
-    headers.forEach(headerText => {
-        let header = document.createElement("th");
-        let textNode = document.createTextNode(headerText);
-        header.appendChild(textNode);
-        headerRow.appendChild(header);
-    });
-
-    contemporaryTable.appendChild(headerRow);
-
-    //Table data
-    for (let i = 0; i < contemporary.length; i++) {
-        let row = "<tr>" + 
-                        "<td>" + "<a href='" + "https://www.google.com'" + ">" + contemporary[i].Culture + "</a>" + "</td>" +
-                        "<td>" + contemporary[i].Affinity + "</td>" +
-                        "<td>" + contemporary[i]["Legacy trait"] + "</td>" +
-                        "<td>" + contemporary[i]["Emblematic unit"] + "</td>" +
-                        "<td>" + contemporary[i]["Emblematic quarter"]+ "</td>" +
-                        "<td>" + contemporary[i]["Affinity action"] + "</td>" +
-                        "<td>" + contemporary[i]["Affinity bonus"] + "</td>" +
-                    "</tr>"
-
-        //Append row to table
-        contemporaryTable.innerHTML += row;
-    };
-
-    //Append to div
-    myTableDiv.appendChild(contemporaryTable);
+    myTableDiv.appendChild(eraTable);
 };
