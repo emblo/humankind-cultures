f30f9289854a67e25a90e616989423fd6534c813
emblo <emma.blomqvist@outlook.com>
2021-10-26 17:16:15 +0200

    process of making it responsive lol

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ window.onload = function() {
     document.getElementById("year").innerHTML = date;
 }
 
+//Hamburger menu
+const hamburger = document.getElementById("hamburger");
+const navUL = document.getElementById("nav-ul");
+
+hamburger.addEventListener("click", () => {
+    navUL.classList.toggle("show");
+});
+
 //Era variables
 const ancient = cultures.filter(obj => { return obj.Era === "Ancient"; });
 const classical = cultures.filter(obj => { return obj.Era === "Classical"; });
